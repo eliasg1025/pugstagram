af2bbaae0865ccc6817003470a26f2be130e551a
Elias Guere <eliasguere1025@gmail.com>
2020-04-17 00:47:36 -0500

    Conectando la aplicacion a una API

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,3 +3,12 @@
         <div class="Footer-copy">&copy; 2020 Pugstagram from Platzi</div>
     </div>
 </div>
+
+<style>
+    .Footer {
+        font-size: 11px;
+        letter-spacing: 1px;
+        font-weight: normal;
+        color: #c7c7c7;
+    }
+</style>
